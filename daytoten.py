3d43ce3fa02b48e687e952cdc4bc1a72d82c4244
Johnny Chang <johnny110982@gmail.com>
2017-08-16 15:42:05 +0000

    First commit

---
+++ daytoten.py
@@ -0,0 +1,322 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 15 11:58:48 2017
+
+@author: Johnny
+"""
+'''
+Day to ten section
+'''
+import pandas as pd
+import csv
+
+input_csv = 'ten.csv'
+full_df = pd.read_csv(input_csv, sep=',', skipinitialspace = True)
+
+Q90 = full_df.Q90
+Q95 = full_df.Q95
+OutBase = full_df.OutBase
+ino = full_df.InandOut
+PWN = full_df.PWN
+
+#Create ten days list
+def daytotenloop(Q90,Q95,OutBase,ino,PWN):
+    Ten90=[]
+    Ten95=[]
+    out=[]
+    inandout=[]
+    pwn=[]
+    
+    for i in range(13):
+        if i == 1:
+            Ten90.append(sum(Q90[0:10]))
+            Ten90.append(sum(Q90[10:20]))
+            Ten90.append(sum(Q90[20:31]))
+            Ten95.append(sum(Q95[0:10]))
+            Ten95.append(sum(Q95[10:20]))
+            Ten95.append(sum(Q95[20:31]))
+            out.append(sum(OutBase[0:10]))
+            out.append(sum(OutBase[10:20]))
+            out.append(sum(OutBase[20:31]))
+            inandout.append(sum(ino[0:10]))
+            inandout.append(sum(ino[10:20]))
+            inandout.append(sum(ino[20:31]))
+            pwn.append(sum(PWN[0:10]))
+            pwn.append(sum(PWN[10:20]))
+            pwn.append(sum(PWN[20:31]))
+        elif i==2:
+            Ten90.append(sum(Q90[31:41]))
+            Ten90.append(sum(Q90[41:51]))
+            Ten90.append(sum(Q90[51:59]))
+            Ten95.append(sum(Q95[31:41]))
+            Ten95.append(sum(Q95[41:51]))
+            Ten95.append(sum(Q95[51:59]))
+            out.append(sum(OutBase[31:41]))
+            out.append(sum(OutBase[41:51]))
+            out.append(sum(OutBase[51:59]))
+            inandout.append(sum(ino[31:41]))
+            inandout.append(sum(ino[41:51]))
+            inandout.append(sum(ino[51:59]))
+            pwn.append(sum(PWN[31:41]))
+            pwn.append(sum(PWN[41:51]))
+            pwn.append(sum(PWN[51:59]))
+        elif i==3:
+            Ten90.append(sum(Q90[59:69]))
+            Ten90.append(sum(Q90[69:79]))
+            Ten90.append(sum(Q90[79:90]))
+            Ten95.append(sum(Q95[59:69]))
+            Ten95.append(sum(Q95[69:79]))
+            Ten95.append(sum(Q95[79:90]))
+            out.append(sum(OutBase[59:69]))
+            out.append(sum(OutBase[69:79]))
+            out.append(sum(OutBase[79:90]))
+            inandout.append(sum(ino[59:69]))
+            inandout.append(sum(ino[69:79]))
+            inandout.append(sum(ino[79:90]))
+            pwn.append(sum(PWN[59:69]))
+            pwn.append(sum(PWN[69:79]))
+            pwn.append(sum(PWN[79:90]))
+        elif i==4:
+            Ten90.append(sum(Q90[90:100]))
+            Ten90.append(sum(Q90[100:110]))
+            Ten90.append(sum(Q90[110:120]))
+            Ten95.append(sum(Q95[90:100]))
+            Ten95.append(sum(Q95[100:110]))
+            Ten95.append(sum(Q95[110:120]))
+            out.append(sum(OutBase[90:100]))
+            out.append(sum(OutBase[100:110]))
+            out.append(sum(OutBase[110:120]))
+            inandout.append(sum(ino[90:100]))
+            inandout.append(sum(ino[100:110]))
+            inandout.append(sum(ino[110:120]))
+            pwn.append(sum(PWN[90:100]))
+            pwn.append(sum(PWN[100:110]))
+            pwn.append(sum(PWN[110:120]))
+        elif i==5:
+            Ten90.append(sum(Q90[120:130]))
+            Ten90.append(sum(Q90[130:140]))
+            Ten90.append(sum(Q90[140:151]))
+            Ten95.append(sum(Q95[120:130]))
+            Ten95.append(sum(Q95[130:140]))
+            Ten95.append(sum(Q95[140:151]))
+            out.append(sum(OutBase[120:130]))
+            out.append(sum(OutBase[130:140]))
+            out.append(sum(OutBase[140:151]))
+            inandout.append(sum(ino[120:130]))
+            inandout.append(sum(ino[130:140]))
+            inandout.append(sum(ino[140:151]))
+            pwn.append(sum(PWN[120:130]))
+            pwn.append(sum(PWN[130:140]))
+            pwn.append(sum(PWN[140:151]))
+        elif i==6:
+            Ten90.append(sum(Q90[151:161]))
+            Ten90.append(sum(Q90[161:171]))
+            Ten90.append(sum(Q90[171:181]))
+            Ten95.append(sum(Q95[151:161]))
+            Ten95.append(sum(Q95[161:171]))
+            Ten95.append(sum(Q95[171:181]))
+            out.append(sum(OutBase[151:161]))
+            out.append(sum(OutBase[161:171]))
+            out.append(sum(OutBase[171:181]))
+            inandout.append(sum(ino[151:161]))
+            inandout.append(sum(ino[161:171]))
+            inandout.append(sum(ino[171:181]))
+            pwn.append(sum(PWN[151:161]))
+            pwn.append(sum(PWN[161:171]))
+            pwn.append(sum(PWN[171:181]))
+        elif i==7:
+            Ten90.append(sum(Q90[181:191]))
+            Ten90.append(sum(Q90[191:201]))
+            Ten90.append(sum(Q90[201:212]))
+            Ten95.append(sum(Q95[181:191]))
+            Ten95.append(sum(Q95[191:201]))
+            Ten95.append(sum(Q95[201:212]))
+            out.append(sum(OutBase[181:191]))
+            out.append(sum(OutBase[191:201]))
+            out.append(sum(OutBase[201:212]))
+            inandout.append(sum(ino[181:191]))
+            inandout.append(sum(ino[191:201]))
+            inandout.append(sum(ino[201:212]))
+            pwn.append(sum(PWN[181:191]))
+            pwn.append(sum(PWN[191:201]))
+            pwn.append(sum(PWN[201:212]))
+        elif i==8:
+            Ten90.append(sum(Q90[212:222]))
+            Ten90.append(sum(Q90[222:232]))
+            Ten90.append(sum(Q90[232:243]))
+            Ten95.append(sum(Q95[212:222]))
+            Ten95.append(sum(Q95[222:232]))
+            Ten95.append(sum(Q95[232:243]))
+            out.append(sum(OutBase[212:222]))
+            out.append(sum(OutBase[222:232]))
+            out.append(sum(OutBase[232:243]))
+            inandout.append(sum(ino[212:222]))
+            inandout.append(sum(ino[222:232]))
+            inandout.append(sum(ino[232:243]))
+            pwn.append(sum(PWN[212:222]))
+            pwn.append(sum(PWN[222:232]))
+            pwn.append(sum(PWN[232:243]))
+        elif i==9:
+            Ten90.append(sum(Q90[243:253]))
+            Ten90.append(sum(Q90[253:263]))
+            Ten90.append(sum(Q90[263:273]))
+            Ten95.append(sum(Q95[243:253]))
+            Ten95.append(sum(Q95[253:263]))
+            Ten95.append(sum(Q95[263:273]))
+            out.append(sum(OutBase[243:253]))
+            out.append(sum(OutBase[253:263]))
+            out.append(sum(OutBase[263:273]))
+            inandout.append(sum(ino[243:253]))
+            inandout.append(sum(ino[253:263]))
+            inandout.append(sum(ino[263:273]))
+            pwn.append(sum(PWN[243:253]))
+            pwn.append(sum(PWN[253:263]))
+            pwn.append(sum(PWN[263:273]))
+        elif i==10:
+            Ten90.append(sum(Q90[273:283]))
+            Ten90.append(sum(Q90[283:293]))
+            Ten90.append(sum(Q90[293:304]))
+            Ten95.append(sum(Q95[273:283]))
+            Ten95.append(sum(Q95[283:293]))
+            Ten95.append(sum(Q95[293:304]))
+            out.append(sum(OutBase[273:283]))
+            out.append(sum(OutBase[283:293]))
+            out.append(sum(OutBase[293:304]))
+            inandout.append(sum(ino[273:283]))
+            inandout.append(sum(ino[283:293]))
+            inandout.append(sum(ino[293:304]))
+            pwn.append(sum(PWN[273:283]))
+            pwn.append(sum(PWN[283:293]))
+            pwn.append(sum(PWN[293:304]))
+        elif i==11:
+            Ten90.append(sum(Q90[304:314]))
+            Ten90.append(sum(Q90[314:324]))
+            Ten90.append(sum(Q90[324:334]))
+            Ten95.append(sum(Q95[304:314]))
+            Ten95.append(sum(Q95[314:324]))
+            Ten95.append(sum(Q95[324:334]))
+            out.append(sum(OutBase[304:314]))
+            out.append(sum(OutBase[314:324]))
+            out.append(sum(OutBase[324:334]))
+            inandout.append(sum(ino[304:314]))
+            inandout.append(sum(ino[314:324]))
+            inandout.append(sum(ino[324:334]))
+            pwn.append(sum(PWN[304:314]))
+            pwn.append(sum(PWN[314:324]))
+            pwn.append(sum(PWN[324:334]))
+        elif i==12:
+            Ten90.append(sum(Q90[334:344]))
+            Ten90.append(sum(Q90[344:354]))
+            Ten90.append(sum(Q90[354:365]))
+            Ten95.append(sum(Q95[334:344]))
+            Ten95.append(sum(Q95[344:354]))
+            Ten95.append(sum(Q95[354:365]))
+            out.append(sum(OutBase[334:344]))
+            out.append(sum(OutBase[344:354]))
+            out.append(sum(OutBase[354:365]))
+            inandout.append(sum(ino[334:344]))
+            inandout.append(sum(ino[344:354]))
+            inandout.append(sum(ino[354:365]))
+            pwn.append(sum(PWN[334:344]))
+            pwn.append(sum(PWN[344:354]))
+            pwn.append(sum(PWN[354:365]))
+    return Ten90,Ten95,out,inandout,pwn
+
+cor_df = daytotenloop(Q90,Q95,OutBase,ino,PWN)
+
+#cor_df = [Ten90,Ten95,out,inandout,pwn]
+#output_csv = 'output.csv'
+#cor_df.to_csv(output_csv, sep=',', encoding='utf-8')
+with open('output.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerows(cor_df)
+
+'''
+DB section
+'''
+import MySQLdb
+import MySQLdb.cursors
+try:
+    conn = MySQLdb.connect(host='localhost',
+                            user='demouser',
+                            passwd='demo1234',
+                            db='demo',
+                            charset='utf8')
+    print("Connect to mysql through demouser.")
+except:
+    print("Can't Connect Database via demouser: ", sys.exc_info()[0])
+    sys.exit()
+    
+cursor = conn.cursor()  
+
+#insert into database
+ten90=cor_df[0]
+ten95=cor_df[1]
+out=cor_df[2]
+inandout=cor_df[3]
+pwn=cor_df[4]
+insert90 = '\"' + '\",\"'.join(map(str, ten90)) + '\"'
+insert95 = '\"' + '\",\"'.join(map(str, ten95)) + '\"'
+insertout = '\"' + '\",\"'.join(map(str, out)) + '\"'
+insertinandout = '\"' + '\",\"'.join(map(str, inandout)) + '\"'
+insertpwn = '\"' + '\",\"'.join(map(str, pwn)) + '\"'
+
+#print('Q90 = '+insert90)
+#print('Q95 = '+insert95)
+#print('In and out = '+insertinandout)
+#print('pwn = '+insertpwn)
+
+#insert to DB
+sql90 = "INSERT INTO Q90 (Ten_1, Ten_2, Ten_3, Ten_4, Ten_5, Ten_6, Ten_7, \
+Ten_8, Ten_9, Ten_10, Ten_11, Ten_12, Ten_13, Ten_14, Ten_15, Ten_16, Ten_17, \
+Ten_18, Ten_19, Ten_20, Ten_21, Ten_22, Ten_23, Ten_24, Ten_25, Ten_26, Ten_27,\
+Ten_28, Ten_29, Ten_30, Ten_31, Ten_32, Ten_33, Ten_34, Ten_35, Ten_36) \
+values ("+insert90+");"
+
+sql95 = "INSERT INTO Q95 (Ten_1, Ten_2, Ten_3, Ten_4, Ten_5, Ten_6, Ten_7, \
+Ten_8, Ten_9, Ten_10, Ten_11, Ten_12, Ten_13, Ten_14, Ten_15, Ten_16, Ten_17, \
+Ten_18, Ten_19, Ten_20, Ten_21, Ten_22, Ten_23, Ten_24, Ten_25, Ten_26, Ten_27,\
+Ten_28, Ten_29, Ten_30, Ten_31, Ten_32, Ten_33, Ten_34, Ten_35, Ten_36) values \
+("+insert95+");"
+
+sqlinandout = "INSERT INTO IrrigationWaterDemand (IrrigationWaterDemand_1,\
+IrrigationWaterDemand_2,IrrigationWaterDemand_3,IrrigationWaterDemand_4,\
+IrrigationWaterDemand_5,IrrigationWaterDemand_6,IrrigationWaterDemand_7,\
+IrrigationWaterDemand_8,IrrigationWaterDemand_9,IrrigationWaterDemand_10,\
+IrrigationWaterDemand_11,IrrigationWaterDemand_12,IrrigationWaterDemand_13,\
+IrrigationWaterDemand_14,IrrigationWaterDemand_15,IrrigationWaterDemand_16,\
+IrrigationWaterDemand_17,IrrigationWaterDemand_18,IrrigationWaterDemand_19,\
+IrrigationWaterDemand_20,IrrigationWaterDemand_21,IrrigationWaterDemand_22,\
+IrrigationWaterDemand_23,IrrigationWaterDemand_24,IrrigationWaterDemand_25,\
+IrrigationWaterDemand_26,IrrigationWaterDemand_27,IrrigationWaterDemand_28,\
+IrrigationWaterDemand_29,IrrigationWaterDemand_30,IrrigationWaterDemand_31,\
+IrrigationWaterDemand_32,IrrigationWaterDemand_33,IrrigationWaterDemand_34,\
+IrrigationWaterDemand_35,IrrigationWaterDemand_36) values ("+insertinandout+");"
+
+sqlpwn = "INSERT INTO PeoplesLivelihoodWater (PeoplesLivelihoodWater_1,\
+PeoplesLivelihoodWater_2,PeoplesLivelihoodWater_3,PeoplesLivelihoodWater_4,\
+PeoplesLivelihoodWater_5,PeoplesLivelihoodWater_6,PeoplesLivelihoodWater_7,\
+PeoplesLivelihoodWater_8,PeoplesLivelihoodWater_9,PeoplesLivelihoodWater_10,\
+PeoplesLivelihoodWater_11,PeoplesLivelihoodWater_12,PeoplesLivelihoodWater_13,\
+PeoplesLivelihoodWater_14,PeoplesLivelihoodWater_15,PeoplesLivelihoodWater_16,\
+PeoplesLivelihoodWater_17,PeoplesLivelihoodWater_18,PeoplesLivelihoodWater_19,\
+PeoplesLivelihoodWater_20,PeoplesLivelihoodWater_21,PeoplesLivelihoodWater_22,\
+PeoplesLivelihoodWater_23,PeoplesLivelihoodWater_24,PeoplesLivelihoodWater_25,\
+PeoplesLivelihoodWater_26,PeoplesLivelihoodWater_27,PeoplesLivelihoodWater_28,\
+PeoplesLivelihoodWater_29,PeoplesLivelihoodWater_30,PeoplesLivelihoodWater_31,\
+PeoplesLivelihoodWater_32,PeoplesLivelihoodWater_33,PeoplesLivelihoodWater_34,\
+PeoplesLivelihoodWater_35,PeoplesLivelihoodWater_36) values ("+insertpwn+");"
+
+print('successfully import csv file to mysql!')
+#outbase table is not created
+cursor.execute(sql90)
+cursor.execute(sql95)
+cursor.execute(sqlinandout)
+cursor.execute(sqlpwn)
+
+conn.commit()
+
+# Close DB link
+cursor.close()
+conn.close()
